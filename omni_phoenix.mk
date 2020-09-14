5f4bc317424e3b5c79e9b69dfcf849a1622c8d90
mauronofrio <poronofrio@gmail.com>
2020-09-14 22:25:09 +0200

    Clean up blacklist

--- omni_phoenix.mk
+++ omni_phoenix.mk
@@ -35,11 +35,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.device \
-    ro.product.name \
-    ro.build.product \
-    ro.bootimage.build.date.utc \
-    ro.build.date.utc
-
